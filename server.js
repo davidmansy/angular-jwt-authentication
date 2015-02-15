374b1b955ea27607afab63163658357895bcdda3
David Mansy <david.mansy@icloud.com>
2015-02-15 12:08:49 +0100

    Auth Interceptor in Angular for JWT

--- server.js
+++ server.js
@@ -4,10 +4,17 @@
 var express = require('express');
 var faker = require('faker');
 var cors = require('cors');
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+var jwtSecret = 'lyuygiuyq8718/1535trd[]';
+var user = {
+  username: 'david',
+  password: 'test'
+};
 
 var app = express();
-
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/random-user', function (req, res) {
   var user = faker.helpers.userCard();
@@ -15,6 +22,31 @@ app.get('/random-user', function (req, res) {
   res.json(user);
 });
 
+app.post('/login', authenticate, function (req, res) {
+  var token = jwt.sign({
+    username: user.username
+  }, jwtSecret);
+  res.send({
+    token: token,
+    user: user
+  });
+});
+
+
 app.listen(3000, function () {
   console.log('App listening on localhost:3000');
 });
+
+// UTIL FUNCTIONS
+function authenticate(req, res, next) {
+  var body = req.body;
+  if (!body.username || !body.password) {
+    res.status(400).end('Must provide username and password!');
+  }
+  else if (body.username !== user.username || body.password !== user.password) {
+    res.status(401).end('Username or password is incorrect.');
+  } else {
+    next();
+  }
+
+}
